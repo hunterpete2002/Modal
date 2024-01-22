63fb13cc82d19caeddf8e389e5390ebcc7429dfe
Petri Kilpelainen <hunterpete2002@gmail.com>
2024-01-22 17:24:23 +0200

    Tehtävä

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable, Modal } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}>
+          <View style={styles.modal}>
+            <Text> This is indeed a modal</Text>
+            <Text></Text>
+            <Pressable
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Text style={styles.textStyle}>Hide Modal (Pressable)</Text>
+            </Pressable>
+          </View>
+      </Modal>
+      <Pressable
+        onPress={() => setModalVisible(true)}>
+        <Text style={styles.textStyle}>Show Modal (Pressable)</Text>
+      </Pressable>
+
     </View>
   );
 }
@@ -17,4 +39,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  modal: {
+    backgroundColor: 'gray',
+    alignItems: 'center',
+    padding: 30,
+    top: 300,
+  }
 });
